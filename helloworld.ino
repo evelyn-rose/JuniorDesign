e2cbf4374391b870c1701d2cc30e6490e8a8fc53
evelyn-rose <84346136+evelyn-rose@users.noreply.github.com>
2023-06-22 23:59:48 -0400

    Create helloworld.ino
    code from the hello world assignment

---
+++ helloworld.ino
@@ -0,0 +1,27 @@
+void setup() {
+pinMode(2, OUTPUT); // sets the digital pin 2 as output
+pinMode(7, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(9, INPUT);
+}
+void loop() {
+int button = digitalRead(9);
+if(button == HIGH){
+digitalWrite(2, HIGH); // sets the digital pin 2 on
+delay(1000); // waits for a second
+digitalWrite(2, LOW); // sets the digital pin 2 off
+delay(1000); // waits for a second
+digitalWrite(7, HIGH); // sets the digital pin 7 on
+delay(1000); // waits for a second
+digitalWrite(7, LOW); // sets the digital pin 7 off
+delay(1000); // waits for a second
+digitalWrite(8, HIGH); // sets the digital pin 8 on
+delay(1000); // waits for a second
+digitalWrite(8, LOW); // sets the digital pin 8 off
+delay(1000); // waits for a second
+} else {
+digitalWrite(2,LOW);
+digitalWrite(7,LOW);
+digitalWrite(8,LOW);
+}
+}
